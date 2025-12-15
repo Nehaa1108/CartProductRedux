79dc595c67478b907c2282527ffc66910b93a4d8
Nehaa1108 <127198995+Nehaa1108@users.noreply.github.com>
2025-12-15 09:34:54 -0500

    set up for redux toolkits

---
+++ src/Service/Store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const Store = configureStore({
+  // abhi slice me koi bhi code nhi h so for now reducer is empty..
+  reducer:{}
+})
+export default Store;

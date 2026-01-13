677b713e47ab0ac0563b4e81684f7a42b2d3f323
Nehaa1108 <127198995+Nehaa1108@users.noreply.github.com>
2026-01-13 17:51:05 +0530

    update

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const Navbar = () => {
+
+  return (
+    <div className='container'>
+      <h5>Welcome <span>{fname} {lname}</span></h5>
+      </div>
+  )
+}
+
+export default Navbar
